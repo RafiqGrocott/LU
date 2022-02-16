61004afd1d8e052f076f34ca987286621349fd94
RafiqGrocott <99296457+RafiqGrocott@users.noreply.github.com>
2022-02-17 10:34:07 +1300

    New Y/N Checker thing

---
+++ 01_yes_no_function_v1.py
@@ -0,0 +1,26 @@
+
+# Functions go here...
+def yes_no(question):
+    valid = False
+    while not valid:
+        response = input(question).lower()
+
+
+        if response == "yes" or response == "y":
+            response = "yes"
+            return response
+
+        elif response == "no" or response == "n":
+            response = "no"
+            return response
+
+        else:
+            print("Please answer yes / no")
+
+        
+# Main Routine go here
+show_instructions = yes_no ("Have you played the game before? ")
+print("You chose {}".format(show_instructions))
+print()
+having_fun = yes_no ("Are you haviung fun? ")
+print("you said {} to having fun".format(having_fun))
